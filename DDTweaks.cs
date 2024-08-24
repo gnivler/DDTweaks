af904a13aa7a0c772f0097ccf6fc771e82a06ca1
gnivler <gnivil@gmail.com>
2024-08-24 12:13:52 -0600

    add "buyJunk" to show own inventory count in bar events (English only)

--- DDTweaks.cs
+++ DDTweaks.cs
@@ -8,9 +8,9 @@ namespace DDTweaks;
 public class DDTweaks : BaseUnityPlugin
 {
     internal static ManualLogSource Log;
-    internal static ModSettings modSettings = new ();
-    internal static Harmony harmony = new ("DDTweaks");
-    
+    internal static ModSettings modSettings = new();
+    internal static Harmony harmony = new("DDTweaks");
+
     private void Awake()
     {
         // Plugin startup logic
@@ -18,8 +18,9 @@ public class DDTweaks : BaseUnityPlugin
         Log.LogWarning($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded successfully.");
         modSettings.books = Config.Bind("General", "Books usage scaling factor", 1f, "1 is vanilla.  0 is no books needed.  0.25 means only 25% of books are needed (75% reduction)");
         modSettings.tires = Config.Bind("General", "Tires wear scaling factor", 1f, "1 is vanilla, 0 is no wear.  0.25 means 25% tire wear (75% reduction)");
+        modSettings.buyJunk = Config.Bind("General", "Show own current stock when buying junk from bars", true,
+            "Vanilla is false.  True will show you (have 25) of something when considering buying junk");
         // modSettings.extraTraits = Config.Bind("General", "Extra trait slots", 0, "Anything over zero adds extra slots for traits");
         Patches.Patch();
-
     }
 }
