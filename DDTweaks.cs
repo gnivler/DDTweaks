661e4e50022acbdd969e8a1409cb0647c559db82
gnivler <gnivil@gmail.com>
2024-08-24 20:15:28 -0600

    different EasyClose approach

--- DDTweaks.cs
+++ DDTweaks.cs
@@ -23,5 +23,7 @@ public class DDTweaks : BaseUnityPlugin
         modSettings.buyJunk = Config.Bind("General", "Show own stock", true, "Vanilla is false.  True will show you \"(have 25)\" of something when considering buying junk in bars");
         modSettings.easyClose = Config.Bind("General", "Allow easy closing of windows", true, "Allow closing of windows, with right-click for top-most and Esc to close all at once");
         Patches.Patch();
+        if (modSettings.easyClose.Value)
+            gameObject.AddComponent<EasyCloser>();
     }
 }
