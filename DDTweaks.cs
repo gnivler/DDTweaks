952fa48684026fcaa3a7d51e478d3be5f4811a65
gnivler <gnivil@gmail.com>
2024-08-26 14:34:27 -0600

    wording

--- DDTweaks.cs
+++ DDTweaks.cs
@@ -21,10 +21,10 @@ public class DDTweaks : BaseUnityPlugin
         Log.LogWarning($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded successfully.");
         modSettings.books = Config.Bind("General", "Books usage scaling factor", 1f, "1 is vanilla.  0 is no books needed.  0.25 means only 25% of books are needed (75% reduction)");
         modSettings.tires = Config.Bind("General", "Tires wear scaling factor", 1f, "1 is vanilla, 0 is no wear.  0.25 means 25% tire wear (75% reduction)");
-        modSettings.buyJunk = Config.Bind("General", "Show own stock", true, "Vanilla is false.  True will show you \"(have 25)\" of something when considering buying junk in bars");
+        modSettings.buyJunk = Config.Bind("General", "Show own stock", true, "Vanilla is false.  True will show you \"(have 25)\" of something when considering buying junk");
         modSettings.easyClose = Config.Bind("General", "Allow easy closing of windows", true, "Allow closing of windows, with right-click for top-most and Esc to close all at once");
         modSettings.quickCombat = Config.Bind("General", "Enter to fight", true, "Executes a coup-de-gras if it's safe, otherwise does a full volley if you have the ammo");
-        modSettings.itemRarity = Config.Bind("General", "Item qualities", true, "The whole item will be blue or purple or whatever instead of a tag being append to it");
+        modSettings.itemRarity = Config.Bind("General", "Item qualities", true, "Superior equipment and food strings are themselves coloured instead of tagged with a modifier word that is coloured");
         Patches.Patch();
         if (modSettings.easyClose.Value)
             gameObject.AddComponent<EasyCloser>();
